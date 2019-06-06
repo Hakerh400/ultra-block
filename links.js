413f90529f616f378e89c8110c494cfad18ce5b8
Thomas <hakerh403@gmail.com>
2019-06-06 02:48:44 +0200

    commit

--- links.js
+++ links.js
@@ -16,12 +16,20 @@
       ee = document.querySelectorAll('a:not(*[target=""]),form:not(*[target=""])');
       for(i = 0; i < ee.length; i++){
         e = ee[i];
-
-        e.target = '';
+        
+        try{
+          e.removeAttribute('target');
+        }catch{}
+        
         e.style.setProperty('opacity', '1', 'important');
       }
 
       setTimeout(block, 1e3);
     }
-  };
+  }
+
+  function log(...a){
+    console.log(...a);
+    return a[a.length - 1];
+  }
 })();
