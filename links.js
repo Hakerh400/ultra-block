33bc47b34d85af5b6c083ce32df49eb6110e0cf6
Thomas <hakerh403@gmail.com>
2019-11-14 11:38:27 +0100

    commit

--- links.js
+++ links.js
@@ -1,31 +1,72 @@
 (() => {
   'use strict';
 
-  if(window.location.href.startsWith('http://localhost/')) return;
+  const {href} = top.location;
+  
+  let stage = 0;
+
+  document.addEventListener('DOMContentLoaded', () => {
+    stage = 1;
+  });
 
   main();
 
   function main(){
-    if(!document.body) return setTimeout(main);
+    if(!document.body)
+      return setTimeout(main);
 
-    block();
-
-    function block(){
-      var ee, e, i, j;
-
-      ee = document.querySelectorAll('a:not(*[target=""]),form:not(*[target=""])');
-      for(i = 0; i < ee.length; i++){
-        e = ee[i];
-        
-        try{
+    const block = () => {
+      if(stage === 0){
+        for(const e of qsa('a,form'))
           e.removeAttribute('target');
-        }catch{}
-        
-        e.style.setProperty('opacity', '1', 'important');
       }
 
-      setTimeout(block, 1e3);
+      for(const e of qsa('a:not(.ublock-safe-link)')){
+        const {href} = e;
+
+        e.removeAttribute('href');
+        show(e);
+
+        e.addEventListener('mousedown', evt => {
+          e.href = href;
+          setTimeout(() => e.removeAttribute('href'), 1e3);
+        });
+      }
+
+      setTimeout(() => requestAnimationFrame(block), 100);
+    };
+
+    block();
+  }
+
+  function show(e){
+    e.classList.add('ublock-safe-link');
+  }
+
+  function qs(a, b=null){
+    if(b === null){
+      b = a;
+      a = document;
     }
+
+    return a.querySelector(b);
+  }
+
+  function qsa(a, b=null){
+    if(b === null){
+      b = a;
+      a = document;
+    }
+
+    return a.querySelectorAll(b);
+  }
+
+  function decode(str){
+    return str.split('').
+      map(a => a.charCodeAt(0) - 32).
+      map(a => 94 - a).
+      map(a => String.fromCharCode(32 + a)).
+      join('');
   }
 
   function log(...a){
