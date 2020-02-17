e491c0f77c7a821c529b0720db9383ff92644dbf
Thomas <hakerh403@gmail.com>
2020-02-17 20:40:31 +0100

    commit

--- google.js
+++ google.js
@@ -7,6 +7,108 @@
     stage = 1;
   });
 
+  const blackList = [
+    'stock',
+    'depositphotos',
+    'gograph',
+    'ftcdn',
+    'alamy',
+    'dreamstime',
+    'featurepics',
+    'carolweaver',
+    'fastly',
+    'fotosearch',
+    'freepik',
+    'whoa',
+    'wallpaper',
+    '123rf',
+    'envato',
+    'gettyimages',
+    'freeimages',
+    'colourbox',
+    'cloudfront',
+    'fotolibra',
+    'photobucket',
+    'onepixel',
+    'foap',
+    'glossboudoir',
+    'clipdealer',
+    'pixoto',
+    'canva.com',
+    'clipgoo',
+    'barewalls',
+    'mp-farm',
+    'vexels',
+    'meashots',
+    'marketplace',
+    'sketchuptextureclub',
+    'newvision',
+    'flickr',
+    'dailymail',
+    'cdninstagram',
+    'w3mirchi',
+    'astrologypandit',
+    'lookaside',
+    'crystalandcomp',
+    'pinimg',
+    'blogspot',
+    'daz3d',
+    'wikihow',
+    'uidownload',
+    'secondlife',
+    'artstation',
+    'cgivault',
+    'etsystatic',
+    'wordpress',
+    'patreon',
+    'shadywady',
+    'leadyourlove',
+    'epicedgephotography',
+    'brainyquote',
+    'stack.imgur',
+    'uigradients',
+    'techdows',
+    'askvg',
+    'redd.it',
+    'javacodegeeks',
+    'gogeometry',
+    'daily-sudoku',
+    'askbobrankin',
+    'pcrisk.com',
+    'ytimg',
+    'yimg',
+    'alicdn',
+    'narvii',
+    'maxpixel',
+    'sguru',
+    'atgbcentral',
+    'cdn.',
+    'qygjxz',
+    'teamusa',
+    'appuals',
+    'i1.wp',
+    'researchgate',
+    'your-camping',
+    'uspa',
+    'epicgames',
+    'proartinc',
+    'hdtimelapse',
+    'dwh-club',
+    'scitepress',
+    'motorsport',
+    'cariire',
+    'blacksmithscafe',
+    'images.template.net',
+    'averroespress',
+    'musicments',
+    'vweb.info',
+    'rexdixon',
+    'www.arto.com',
+    'cavotagoo',
+    'sciencesource',
+    '.gif&',
+  ];
+
   main();
 
   function main(){
@@ -16,12 +118,9 @@
     block();
 
     function block(){
-      var e;
+      let e;
 
-      if(stage === 0)
-        return setTimeout(block, 1e3);
-
-      for(e of qsa('.g')){
+      for(let e of qsa('.g')){
         if(e.innerHTML.includes('<span>Missing:</span>'))
           e.remove();
       }
@@ -33,7 +132,29 @@
           e.remove();
         }
       }
+
+      for(const e of qsa('.islrc > div:not(.ublock-safe)')){
+        let found = 0;
+
+        for(const link of qsa(e, 'a')){
+          const url = link.href.toLowerCase();
+
+          if(blackList.some(a => url.includes(a))){
+            found = 1;
+            break;
+          }
+        }
+
+        if(found) e.remove();
+        else show(e);
+      }
+
+      setTimeout(block, stage ? 1e3 : 0);
     }
+  }
+
+  function show(e){
+    e.classList.add('ublock-safe');
   }
 
   function qs(a, b=null){
